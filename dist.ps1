af98f3867dd5679b346c80000050af3c65b026ce
ShreckedTO <58538423+ShreckedTO@users.noreply.github.com>
2021-12-13 20:14:01 -0800

    Add files via upload

---
+++ dist.ps1
@@ -0,0 +1,18 @@
+$ver = Get-Content ver.txt
+$ver = ([int]$ver + 1)
+Write-Output $ver
+
+Write-Output "Preparing nerw.js for distribution..."
+Write-Output "Creating nerw-docs.js file"
+terser --compress --mangle --comments some nerw.js -o "dist/nerw-docs-v$ver.js"
+Write-Output "Created nerw-docs.js file"
+Write-Output "Creating nerw.js file"
+terser --compress --mangle --comments false nerw.js -o "dist/nerw-nohead-v$ver.js"
+Write-Output "Created nerw.js file"
+# Start-Sleep -Milliseconds 2000
+Write-Output "Adding header to compressed dist"
+Get-Content header.txt, "dist/nerw-nohead-v$ver.js" | Set-Content "dist/nerw-v$ver.js"
+Write-Output "Added header to compressed dist"
+Write-Output "echo nerw.js dist files writted in dist folder"
+
+[String]$ver | Out-File -FilePath ver.txt
---
+++ nerw.js
@@ -0,0 +1,466 @@
+/**
+ * 
+ * NERW.js - A lightweight neural network library written in javascript
+ * Visit me at https://shrecked.my.to/
+ * 
+ * @module nerwjs
+ * @preserve
+ */
+
+var loadAlerts = false;
+if (loadAlerts) console.log("[NERWJS] Loading nerwjs");
+var startLoadTime = new Date().getTime();
+
+/**
+ * Network node type enum
+ * @readonly
+ * @enum {Symbol}
+ * @preserve
+*/
+var NodeType = {
+    INPUT: Symbol("networkNodeInput"),
+    LAYER: Symbol("networkNodeLayer"),
+    OUTPUT: Symbol("networkNodeOutput")
+};
+
+/**
+ * Activation function override enum
+ * @readonly
+ * @enum {Symbol}
+ * @preserve
+*/
+var ActivationOverride = {
+    NONE: Symbol("noneActivationOverride"),
+    SIGMOID: Symbol("sigmoidActivationOverride"),
+    TANH: Symbol("tanhActivationOverride"),
+    RELU: Symbol("ReLUActivationOverride"),
+    LEAKYRELU: Symbol("LeakyReLUActivationOverride")
+}
+
+/**
+ * Sorting algorithm enum
+ * @readonly
+ * @enum {Symbol}
+ * @preserve
+*/
+var SortingAlgorithmOverride = {
+    NONE: Symbol("noneSortingAlgorithmOverride"),
+    SELECTION: Symbol("selectionSortingAlgorithmOverride"),
+    BUBBLE: Symbol("bubbleSortingAlgorithmOverride")
+}
+
+/**
+ * Sorting algorithm functions
+ * @preserve
+ */
+var SortingAlgorithm = {
+    selectionSort: function(a,b){let c=a.length;for(let d,e=0;e<c;e++){d=e;for(let f=e+1;f<c;f++)a[f][b]<a[d][b]&&(d=f);if(d!=e){let b=a[e];a[e]=a[d],a[d]=b}}return a},
+    bubbleSort: function(a,b){let c=a.length;for(let d=0;d<c;d++)for(let d=0;d<c;d++)if(a[d][b]>a[d+1][b]){let b=a[d];a[d]=a[d+1],a[d+1]=b}return a}
+};
+
+/**
+ * Activation function definitions
+ * @preserve
+ * */
+var ActivationFunction = {
+    RELU: function(x) {
+        return Math.max(x,0);
+    },
+    LEAKYRELU: function(x) {
+        return Math.max(x,x/100);
+    },
+    SIGMOID: function(x) {
+        return 1/(1+Math.pow(2.71828182846,-x));
+    }
+}
+
+/**
+ * Class representing a single neural network
+ * @class
+ * @preserve
+ */
+class Network {
+    /**
+     * Create a neural network
+     * @constructor
+     * @preserve
+     */
+    constructor() {
+        this.inputs = [];
+        this.layers = [];
+        this.outputs = [];
+        this.score = 0;
+        this.initiated = false;
+        this.weightCount = 0;
+    }
+    
+    /**
+     * Add an input node
+     * @param {string} input - The name of the input node
+     * @preserve
+     */
+    addInput(input) {
+        if (this.initiated) return new Error("Network is already initiated");
+        if (this.inputs.includes(input)) throw new Error("Network already has input named \"" + input + "\"");
+        this.inputs.push(input);
+        //console.log("add input " + input);
+        this.weightCount++;
+    }
+    
+    /**
+     * Add a hidden layer
+     * @param {string} layer - The name of the hidden layer
+     * @param {number} nc - The ammount of nodes in the hidden layer
+     * @preserve
+     */
+    addLayer(layer,nc) {
+        if (this.initiated) return new Error("Network is already initiated");
+        if (this.layers.includes(layer)) throw new Error("Network already has layer named \"" + layer + "\"");
+        this.layers.push([layer,nc,this.weightCount]);
+        this.weightCount = nc;
+        //console.log("add layer " + layer + " with " + nc + " nodes");
+    }
+    
+    /**
+     * Add an output node
+     * @param {string} output - The name of the output node
+     * @preserve
+     */
+    addOutput(output) {
+        if (this.initiated) return new Error("Network is already initiated");
+        if (this.outputs.includes(output)) throw new Error("Network already has output named \"" + output + "\"");
+        this.outputs.push([output,this.weightCount]);
+        //console.log("add output " + output);
+    }
+    
+    /**
+     * Initiate the neural network - further changes to the network cannot be made
+     * @preserve
+     */
+    initiate() {
+        if (this.initiated) return new Error("Network is already initiated");
+        for (var i = 0; i < this.layers.length; i++) {
+            var createLayer = this.layers[i];
+            this.layers[i] = [];
+            for (var j = 0; j < createLayer[1]; j++) {
+                this.layers[i].push(new NetworkNode(NodeType.LAYER,createLayer[2]));
+            }
+        }
+        for (i = 0; i < this.outputs.length; i++) {
+            var createOutput = this.outputs[i];
+            this.outputs[i] = new NetworkNode(NodeType.OUTPUT,createOutput[1]);
+        }
+        this.initiated = true;
+        //console.log("initiate");
+    }
+    
+    /**
+     * Pass a value into the neural network
+     * @param {Object} input - Arguments to pass to the neural network
+     * @returns {Object} The output of the network
+     * @preserve
+     */
+    activate() {
+        if (arguments.length != this.inputs.length && typeof(arguments[0]) != "object") throw new Error("Bad input count, expected " + this.inputs.length + ", got " + arguments.length);
+        if (arguments[0].length != this.inputs.length && typeof(arguments[0]) == "object") throw new Error("Bad input count, expected " + this.inputs.length + ", got " + arguments[0].length);
+        var args = [];
+        if (typeof(arguments[0]) == "object") args = arguments[0];
+        if (typeof(arguments[0]) != "object") args = arguments;
+        var layerIn = args;
+        var layerOut = [];
+        for (var i = 0; i < this.layers.length; i++) {
+            for (var j = 0; j < this.layers[i].length; j++) {
+                layerOut.push(this.layers[i][j].activate(layerIn));
+            }
+            layerIn = layerOut;
+            layerOut = [];
+        }
+        for (i = 0; i < this.outputs.length; i++) {
+            layerOut.push(this.outputs[i].activate(layerIn));
+        }
+        return layerOut;
+    }
+    
+    /**
+     * Mutate the network
+     * @preserve
+     */
+    mutate() {
+        for (var i = 0; i < this.layers.length; i++) {
+            for (var j = 0; j < this.layers[i].length; j++) {
+                this.layers[i][j].mutate();
+            }
+        }
+        for (i = 0; i < this.outputs.length; i++) {
+            this.outputs[i].mutate();
+        }
+    }
+    
+    /**
+     * Clones the network
+     * @returns {Network} Returns the cloned network
+     * @preserve
+     */
+    cloneNetwork() {
+        var cloneNetworkObj = new Network();
+        cloneNetworkObj.inputs = this.inputs;
+        for (var i = 0; i < this.layers.length; i++) {
+            cloneNetworkObj.layers[i] = [];
+            for (var j = 0; j < this.layers[i].length; j++) {
+                cloneNetworkObj.layers[i][j] = this.layers[i][j].cloneNode();
+            }
+        }
+        for (i = 0; i < this.outputs.length; i++) {
+            cloneNetworkObj.outputs[i] = this.outputs[i].cloneNode();
+        }
+        cloneNetworkObj.initiated = this.initiated;
+        cloneNetworkObj.score = this.score;
+        return cloneNetworkObj;
+    }
+}
+
+/**
+ * Class representing a network node
+ * @class
+ * @preserve
+ */
+class NetworkNode {
+    /**
+     * Create a network node
+     * @param {Symbol} nodeType - The type of the network node
+     * @param {number} weightCount - The ammount of weight variables
+     * @constructor
+     * @preserve
+     */
+    constructor(nodeType, weightCount) {
+        if (!(nodeType == NodeType.INPUT || nodeType == NodeType.LAYER || nodeType == NodeType.OUTPUT)) throw new Error("Unknown node type " + nodeType.toString());
+        this.weightCount = weightCount;
+        this.type = nodeType;
+        this.activationOverride = ActivationOverride.NONE;
+        this.weights = [];
+        this.bias = Math.random();
+        for (var i = 0; i < weightCount; i++) {
+            this.weights.push(Math.random());
+        }
+    }
+    
+    /**
+     * Override the activation function for this node
+     * @param {Symbol} activationOverride - The type of activation function to use
+     * @preserve
+     */
+    overrideActivation(activationOverride) {
+        for (var i in Object.keys(ActivationOverride)) {
+            if (ActivationOverride[Object.keys(ActivationOverride)[i]] == activationOverride) {
+                this.activationOverride = activationOverride;
+                return;
+            }
+        }
+        throw new Error("Unknown activation override type " + activationOverride.toString());
+    }
+    
+    /**
+     * Activate the network node
+     * @param {Object} aco - Array of inputs
+     * @returns {number} Returns the activated product
+     * @preserve
+     */
+    activate(aco) {
+        var ac = 0;
+        for (var i in aco) {
+            ac += aco[i] * this.weights[i];
+        }
+        ac += this.bias;
+        
+        if (this.activationOverride != ActivationOverride.NONE) {
+            switch (this.activationOverride) {
+                case ActivationOverride.SIGMOID:
+                    return ActivationFunction.SIGMOID(ac);
+                case ActivationOverride.TANH:
+                    return ActivationFunction.TANH(ac);
+                case ActivationOverride.RELU:
+                    return ActivationFunction.RELU(ac);
+                case ActivationOverride.LEAKYRELU:
+                    return ActivationFunction.LEAKYRELU(ac);
+                default:
+                    throw new Error("Unknown activation override type " + this.activationOverride.toString());
+            }
+        } else {
+            switch (this.type) {
+                case NodeType.INPUT:
+                    return ActivationFunction.RELU(ac);
+                case NodeType.LAYER:
+                    return ActivationFunction.LEAKYRELU(ac);
+                case NodeType.OUTPUT:
+                    return ActivationFunction.SIGMOID(ac);
+                default:
+                    throw new Error("Unknown node type " + this.type.toString());
+            }
+        }
+        //return ac;
+    }
+    
+    /**
+     * Mutate the network node
+     * @preserve
+     */
+    mutate() {
+        for (var i = 0; i < this.weights.length; i++) {
+            this.weights[i] += (Math.random() - 0.5) * 0.1;
+        }
+        this.bias += (Math.random() - 0.5) * 0.1;
+    }
+
+    /**
+     * 
+     * @param {Object} obj 
+     * @returns {Object} Returns the cloned object
+     * @preserve
+     */
+    clone(obj) {
+        if (null == obj || "object" != typeof obj) return obj;
+        var copy = new obj.constructor();
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+        }
+        return copy;
+    }
+
+    /**
+     * Clone the network node
+     * @returns {NetworkNode} Returns the cloned network node
+     * @preserve
+     */
+    cloneNode() {
+        var cloneNodeObj = new NetworkNode(this.type, this.weightCount);
+        cloneNodeObj.activationOverride = this.activationOverride;
+        cloneNodeObj.weights = this.clone(this.weights);
+        cloneNodeObj.bias = this.bias;
+        return cloneNodeObj;
+    }
+}
+
+/**
+ * Class representing a group of neural networks
+ * @class
+ * @preserve
+ */
+class NetworkGroup {
+    /**
+     * Create a network group
+     * @param {number} amnt - The amount of networks to be created
+     * @constructor
+     * @preserve
+     */
+    constructor(amnt) {
+        this.networks = [];
+        for (var i = 0; i < amnt; i++) {
+            this.networks.push(new Network());
+        }
+        this.initiated = false;
+    }
+
+    /**
+     * Add an input node to all of the networks
+     * @param {string} input - The name of the input node
+     * @preserve
+     */
+    addInput(input) {
+        if (this.initiated) throw new Error("Network is already initiated");
+        for (var i = 0; i < this.networks.length; i++) {
+            this.networks[i].addInput(input);
+        }
+    }
+
+    /**
+     * Add a hidden layer to all of the networks
+     * @param {string} layer - The name of the hidden layer
+     * @param {number} nc - The ammount of nodes in the hidden layer
+     * @preserve
+     */
+    addLayer(layer,nc) {
+        if (this.initiated) throw new Error("Network is already initiated");
+        for (var i = 0; i < this.networks.length; i++) {
+            this.networks[i].addLayer(layer,nc);
+        }
+    }
+
+    /**
+     * Add an output node to all of the networks
+     * @param {string} output - The name of the output node
+     * @preserve
+     */
+    addOutput(output) {
+        if (this.initiated) throw new Error("Network is already initiated");
+        for (var i = 0; i < this.networks.length; i++) {
+            this.networks[i].addOutput(output);
+        }
+    }
+
+    /**
+     * Initiate all of the neural networks - further changes to the networks cannot be made
+     * @preserve
+     */
+    initiate() {
+        if (this.initiated) throw new Error("Network is already initiated");
+        for (var i = 0; i < this.networks.length; i++) {
+            this.networks[i].initiate();
+        }
+        this.initiated = true;
+    }
+
+    /**
+     * Execute a function for each of the networks
+     * @param {eachCallback} cb - A function to execute for each of the networks
+     * @preserve
+     */
+    each(cb) {
+        for (var i = 0; i < this.networks.length; i++) {
+            cb(this.networks[i],i);
+        }
+    }
+    /**
+     * A callback for each of the networks in a group
+     * @callback eachCallback
+     * @param {Network} network
+     * @param {number} interval
+     * @preserve
+     */
+
+    /**
+     * Sort the networks in the group based on their score
+     * @returns {NetworkGroup} The sorted network group
+     * @preserve
+     */
+    sort() {
+        this.networks = SortingAlgorithm.selectionSort(this.networks, "score");
+        return this.networks;
+    }
+
+    /**
+     * Replace the worst networks with a mutated version of a better network
+     * @preserve
+     */
+    generation() {
+        var offset = Math.round(this.networks.length/2);
+        for (var i = 0; i < offset; i++) {
+            this.networks[i+offset] = this.networks[i].cloneNetwork();
+            this.networks[i+offset].mutate();
+        }
+    }
+}
+
+// If in NodeJS, add to exports
+if (typeof exports != "undefined") {
+    if (loadAlerts) console.log("[NERWJS] Adding nerwjs to exports");
+    exports.NodeType = NodeType;
+    exports.ActivationOverride = ActivationOverride;
+    exports.SortingAlgorithmOverride = SortingAlgorithmOverride;
+    exports.SortingAlgorithm = SortingAlgorithm;
+    exports.ActivationFunction = ActivationFunction;
+    exports.Network = Network;
+    exports.NetworkNode = NetworkNode;
+    exports.NetworkGroup = NetworkGroup;
+}
+
+if (loadAlerts) console.log("[NERWJS] Loaded nerwjs in " + (new Date().getTime()-startLoadTime) + " ms");
